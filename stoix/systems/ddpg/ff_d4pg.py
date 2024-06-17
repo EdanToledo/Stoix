2f57ae67bcb81c8c9776276595a8b2fad65d5e2f
EdanToledo <edan.toledo@live.com>
2024-06-17 17:17:53 +0000

    fix: add nstep transitions to d4pg

--- stoix/systems/ddpg/ff_d4pg.py
+++ stoix/systems/ddpg/ff_d4pg.py
@@ -39,6 +39,7 @@ from stoix.utils.checkpointing import Checkpointer
 from stoix.utils.jax_utils import unreplicate_batch_dim, unreplicate_n_dims
 from stoix.utils.logger import LogEvent, StoixLogger
 from stoix.utils.loss import categorical_td_learning
+from stoix.utils.multistep import batch_discounted_returns
 from stoix.utils.total_timestep_checker import check_total_timesteps
 from stoix.utils.training import make_learning_rate
 from stoix.wrappers.episode_metrics import get_final_step_metrics
@@ -104,6 +105,8 @@ def get_warmup_fn(
         )
 
         # Add the trajectory to the buffer.
+        # Swap the batch and time axes.
+        traj_batch = jax.tree_util.tree_map(lambda x: jnp.swapaxes(x, 0, 1), traj_batch)
         buffer_states = buffer_add_fn(buffer_states, traj_batch)
 
         return env_states, timesteps, keys, buffer_states
@@ -170,6 +173,8 @@ def get_learner_fn(
         params, opt_states, buffer_state, key, env_state, last_timestep = learner_state
 
         # Add the trajectory to the buffer.
+        # Swap the batch and time axes.
+        traj_batch = jax.tree_util.tree_map(lambda x: jnp.swapaxes(x, 0, 1), traj_batch)
         buffer_state = buffer_add_fn(buffer_state, traj_batch)
 
         def _update_epoch(update_state: Tuple, _: Any) -> Tuple:
@@ -242,7 +247,29 @@ def get_learner_fn(
 
             # SAMPLE TRANSITIONS
             transition_sample = buffer_sample_fn(buffer_state, sample_key)
-            transitions: Transition = transition_sample.experience
+            transition_sequence: Transition = transition_sample.experience
+            # Extract the first and last observations.
+            step_0_obs = jax.tree_util.tree_map(lambda x: x[:, 0], transition_sequence).obs
+            step_0_actions = transition_sequence.action[:, 0]
+            step_n_obs = jax.tree_util.tree_map(lambda x: x[:, -1], transition_sequence).next_obs
+            # check if any of the transitions are done - this will be used to decide
+            # if bootstrapping is needed
+            n_step_done = jnp.any(transition_sequence.done, axis=-1)
+            # Calculate the n-step rewards and select the first one.
+            discounts = 1.0 - transition_sequence.done.astype(jnp.float32)
+            n_step_reward = batch_discounted_returns(
+                transition_sequence.reward,
+                discounts * config.system.gamma,
+                jnp.zeros_like(discounts),
+            )[:, 0]
+            transitions = Transition(
+                obs=step_0_obs,
+                action=step_0_actions,
+                reward=n_step_reward,
+                done=n_step_done,
+                next_obs=step_n_obs,
+                info=transition_sequence.info,
+            )
 
             # CALCULATE ACTOR LOSS
             actor_grad_fn = jax.grad(_actor_loss_fn, has_aux=True)
@@ -326,7 +353,6 @@ def get_learner_fn(
         This function represents the learner, it updates the network parameters
         by iteratively applying the `_update_step` function for a fixed number of
         updates. The `_update_step` function is vectorized over a batch of inputs.
-
         """
 
         batched_update_step = jax.vmap(_update_step, in_axes=(0, None), axis_name="batch")
@@ -449,12 +475,14 @@ def learner_setup(
     config.system.batch_size = config.system.total_batch_size // (
         n_devices * config.arch.update_batch_size
     )
-    buffer_fn = fbx.make_item_buffer(
-        max_length=config.system.buffer_size,
-        min_length=config.system.batch_size,
+
+    buffer_fn = fbx.make_trajectory_buffer(
+        max_size=config.system.buffer_size,
+        min_length_time_axis=config.system.n_step,
         sample_batch_size=config.system.batch_size,
-        add_batches=True,
-        add_sequences=True,
+        add_batch_size=config.arch.num_envs,
+        sample_sequence_length=config.system.n_step,
+        period=1,
     )
     buffer_fns = (buffer_fn.add, buffer_fn.sample)
     buffer_states = buffer_fn.init(dummy_transition)
@@ -473,9 +501,12 @@ def learner_setup(
     env_states, timesteps = jax.vmap(env.reset, in_axes=(0))(
         jnp.stack(env_keys),
     )
-    reshape_states = lambda x: x.reshape(
-        (n_devices, config.arch.update_batch_size, config.arch.num_envs) + x.shape[1:]
-    )
+
+    def reshape_states(x: chex.Array) -> chex.Array:
+        return x.reshape(
+            (n_devices, config.arch.update_batch_size, config.arch.num_envs) + x.shape[1:]
+        )
+
     # (devices, update batch size, num_envs, ...)
     env_states = jax.tree_util.tree_map(reshape_states, env_states)
     timesteps = jax.tree_util.tree_map(reshape_states, timesteps)
@@ -495,14 +526,19 @@ def learner_setup(
     key, step_key, warmup_key = jax.random.split(key, num=3)
     step_keys = jax.random.split(step_key, n_devices * config.arch.update_batch_size)
     warmup_keys = jax.random.split(warmup_key, n_devices * config.arch.update_batch_size)
-    reshape_keys = lambda x: x.reshape((n_devices, config.arch.update_batch_size) + x.shape[1:])
+
+    def reshape_keys(x: chex.PRNGKey) -> chex.PRNGKey:
+        return x.reshape((n_devices, config.arch.update_batch_size) + x.shape[1:])
+
     step_keys = reshape_keys(jnp.stack(step_keys))
     warmup_keys = reshape_keys(jnp.stack(warmup_keys))
 
     replicate_learner = (params, opt_states, buffer_states)
 
     # Duplicate learner for update_batch_size.
-    broadcast = lambda x: jnp.broadcast_to(x, (config.arch.update_batch_size,) + x.shape)
+    def broadcast(x: chex.Array) -> chex.Array:
+        return jnp.broadcast_to(x, (config.arch.update_batch_size,) + x.shape)
+
     replicate_learner = jax.tree_util.tree_map(broadcast, replicate_learner)
 
     # Duplicate learner across devices.
