5f020db97bf2c762aaafdb597ab8300e879a9149
edantoledo <edan.toledo@live.com>
2025-03-26 17:54:28 +0000

    fix: ppo and ppo continuous to use new gae calc

--- stoix/systems/ppo/ppo_types.py
+++ stoix/systems/ppo/ppo_types.py
@@ -14,6 +14,7 @@ class PPOTransition(NamedTuple):
     action: Action
     value: Value
     reward: chex.Array
+    bootstrap_value: Value
     log_prob: chex.Array
     obs: chex.Array
     info: Dict
